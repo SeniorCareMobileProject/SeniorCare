195439ed00640cb48992e49f7ef52ccb0b4135e2
andreistr21 <60103919+andreistr21@users.noreply.github.com>
2022-12-19 19:31:17 +0100

    Fixed default safe zone radius show instead of previous

--- app/src/main/java/com/SeniorCareMobileProject/seniorcare/ui/views/Atoms/Maps.kt
+++ app/src/main/java/com/SeniorCareMobileProject/seniorcare/ui/views/Atoms/Maps.kt
@@ -121,6 +121,11 @@ fun RadiusChanger(navController: NavController, sharedViewModel: SharedViewModel
             context.packageName
         )
     }
+
+    if (sharedViewModel.geofenceRadius.value > 1) {
+        sharedViewModel.newGeofenceRadius.value = sharedViewModel.geofenceRadius.value
+    }
+
     Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
